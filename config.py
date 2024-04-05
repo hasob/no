2d9e81c4a98e077375f4b597c9c67ac5efa9a870
hasob <110573422+hasob@users.noreply.github.com>
2024-04-06 01:36:30 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6496914805:AAFo1MQA3rPsrsoGEa7g8kTUBCgefKLIRR4")
-
-    APP_ID = int(os.environ.get("APP_ID", 23062240))
-
-    API_HASH = os.environ.get("API_HASH", "582a897781f43febc47cde342cbe2792")
