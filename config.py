9270e28bcc30850bf1cea35a8c5553a440a108f1
hasob <110573422+hasob@users.noreply.github.com>
2024-04-06 03:12:56 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Config(object):
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7078356954:AAGKmIXox34cWrlDPNLoRDLA3jK82ufl60Y")
+
+    APP_ID = int(os.environ.get("APP_ID", "23062240"))
+
+    API_HASH = os.environ.get("API_HASH", "582a897781f43febc47cde342cbe2792")
